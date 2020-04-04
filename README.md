48dbb3e7b1994dc4c52e7287be4f4e629aba6e7c
shanx <shanx@git.com>
2020-04-04 18:45:25 +0200

    only left push bottom row working

---
+++ README.md
@@ -0,0 +1,9 @@
+#2048
+
+Press 4 to swipe left
+Press 6 to swipe right
+Press 8 to swipe up
+Press 2 to swipe down
+
+Steps to run: 
+./a.out
---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include <iostream> 
+#include <stdlib.h>
+#include <random>
+#include <ctime>
+#include <chrono>
+
+namespace randGen {
+    std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
+}
+
+int generateRandomNumber(int min, int max){
+
+    std::uniform_int_distribution<> gen{min, max};
+    int slot_one = gen(randGen::mersenne);
+    if(slot_one == 3){
+        slot_one--; 
+    }
+    return slot_one; 
+}
+
+int printscreen(int *p){
+    std::cout<<"\n======================================================\n";
+    std::cout<<*p<<"\t\t\t"<<*(p+1)<<"\t\t\t"<<*(p+2); 
+    std::cout<<"\n======================================================\n";
+    std::cout<<*(p+3)<<"\t\t\t"<<*(p+4)<<"\t\t\t"<<*(p+5); 
+    std::cout<<"\n======================================================\n";
+    std::cout<<*(p+6)<<"\t\t\t"<<*(p+7)<<"\t\t\t"<<*(p+8); 
+    std::cout<<"\n======================================================\n\n\n";
+    return 0; 
+}
+
+/*
+int leftwards(uint arr[3][3], int temp[3]){
+    std::cout<<"entered leftwards"; 
+    int temp0 = 0, temp1 = 0, temp2 = 0;
+    uint *p = &arr[0][0]; 
+
+    for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 1; j++){
+                    arr[i][j] = temp[i] + arr[i][j+1] + arr[i][j+2]; 
+                }
+                temp[i] = arr[i][0]; 
+            }
+            arr[2][2] = generateRandomNumber(2, 4); 
+            //printscreen(p); 
+}
+*/
+
+int leftPush(int *p){
+/*
+    if((*p == 0) && (*(p+1) == 0)){
+        *p = *(p+2);
+    }
+
+    if((*p != 0) && (*p == *(p+1))){
+        *p = *p + *(p+1); 
+        *(p+1) = *(p+2); 
+    }else if ((*p != 0) && (*(p+1) == *(p+2))){
+        *(p+1) = *(p+1) + *(p+2); 
+        *(p+2) = 0; 
+    }
+    std::cout<<*p; 
+
+    if((*(p+3) == 0) && (*(p+4) == 0)){
+        *(p+3) = *(p+5);
+    }
+
+    if(*(p+3) == *(p+4)){
+    *(p+3) = *(p+3) + *(p+4); 
+    *(p+4) = *(p+5); 
+    }else if (*(p+4) == *(p+5)){
+    *(p+4) = *(p+4) + *(p+5); 
+    *(p+5) = 0; 
+    }
+*/
+std::cout<<"entered leftpush"; 
+   // for(int i = 0; i <= 6; i+3){
+       int i = 6; 
+        if(*(p+i) == 0){
+            if(*(p+i+1) == *(p+i+2)){
+                *(p+i) = *(p+i+1) + *(p+i+2);
+                *(p+i+1) = 0;
+                *(p+i+2) = 0; 
+            }else if((*(p+i+1) != *(p+i+2))){
+                *(p+i) = *(p+i+1);
+                *(p+i+1) = *(p+i+2); 
+            }
+        }else if(*(p+i) != 0){
+            if(*(p+i) == *(p+i+1)){
+                *(p+i) = *(p+i) + *(p+i+1); 
+                *(p+i+1) = *(p+i+2);
+                *(p+i+2) = 0;  
+            }else if (*(p+i+1) == *(p+i+2)){
+                *(p+i+1) = *(p+i+1) + *(p+i+2); 
+                *(p+i+2) = 0; 
+            }else if ((*(p+i+1) == 0) && (*(p+i) == *(p+i+2))){
+                *(p+i) = *(p+i) + *(p+i+2); 
+            }else if ((*(p+i+1) == 0) && (*(p+i) != *(p+i+2))){
+                *(p+i+1) = *(p+i+2);
+                *(p+i+2) = 0;  
+            }
+        }
+    //}
+    
+    //*(p+7) = generateRandomNumber(2,4); 
+    *(p+8) = generateRandomNumber(2,4); 
+    printscreen(p); 
+/*
+    *p = *p + *(p+1) + *(p+2);
+    *p3 = *p3 + *(p+4) + *(p+5);
+    *p6 = *p6 + *(p+7) + *(p+8);
+    */
+}
+
+int main(){
+    int temp0 = 0, temp1 = 0, temp2 = 0;
+    int temp[3]{}; 
+    static int arr[3][3]{{},{},{0,2,2}};
+    int *p = &arr[0][0]; 
+    //*(p+2) = 3; 
+   // *(p+5) = generateRandomNumber(2,4); 
+    *(p+8) = generateRandomNumber(2,4); 
+    std::cout<<*(p+1);  
+    printscreen(p);    
+    char input; 
+    while(1){
+        std::cin>>input;
+          
+        switch (input)
+        {
+        case 2:     //leftwards(arr[3][3], temp[3]); 
+                    
+        case '4':    leftPush(p);  
+                    break; 
+
+        default: std::cout<<"Invalid option\n"; 
+
+        }
+
+    }
+}
